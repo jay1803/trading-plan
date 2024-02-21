cfde6a790d62d256c428ed85b59ec9781168479a
maxOS <github.com@maxos.email>
2024-02-21 09:16:24 +0800

    use vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Theme } from "@radix-ui/themes";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -19,5 +20,6 @@ root.render(
                 <App />
             </Theme>
         </ThemeProvider>
+        <Analytics />
     </React.StrictMode>
 );
